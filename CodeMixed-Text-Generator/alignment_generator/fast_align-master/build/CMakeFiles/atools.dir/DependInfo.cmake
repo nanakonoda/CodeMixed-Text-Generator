76977a7e7c5b44c4444a62444ddad08df067a0b8
Nanako Noda <nnoda@hmc.edu>
2023-02-28 17:40:27 -0800

    added files to CodeMixed

---
+++ CodeMixed-Text-Generator/alignment_generator/fast_align-master/build/CMakeFiles/atools.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nanako/Desktop/Clinic/testing/CodeMixed-Text-Generator/CodeMixed-Text-Generator/alignment_generator/fast_align-master/src/alignment_io.cc" "CMakeFiles/atools.dir/src/alignment_io.cc.o" "gcc" "CMakeFiles/atools.dir/src/alignment_io.cc.o.d"
+  "/Users/nanako/Desktop/Clinic/testing/CodeMixed-Text-Generator/CodeMixed-Text-Generator/alignment_generator/fast_align-master/src/atools.cc" "CMakeFiles/atools.dir/src/atools.cc.o" "gcc" "CMakeFiles/atools.dir/src/atools.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CodeMixed-Text-Generator/alignment_generator/fast_align-master/build/CMakeFiles/fast_align.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nanako/Desktop/Clinic/testing/CodeMixed-Text-Generator/CodeMixed-Text-Generator/alignment_generator/fast_align-master/src/fast_align.cc" "CMakeFiles/fast_align.dir/src/fast_align.cc.o" "gcc" "CMakeFiles/fast_align.dir/src/fast_align.cc.o.d"
+  "/Users/nanako/Desktop/Clinic/testing/CodeMixed-Text-Generator/CodeMixed-Text-Generator/alignment_generator/fast_align-master/src/ttables.cc" "CMakeFiles/fast_align.dir/src/ttables.cc.o" "gcc" "CMakeFiles/fast_align.dir/src/ttables.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
